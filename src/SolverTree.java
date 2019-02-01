5f029e3d91b6d5eaa94072b93fec167d2878f312
Damien Rodriguez <drodriguez17@eagles.ewu.edu>
2019-02-01 15:31:43 -0800

    clean up w/Solver Tree
    Solver tree created and cleaned up tester bits

---
+++ src/SolverTree.java
@@ -0,0 +1,15 @@
+
+
+public class SolverTree
+{
+    private class Node
+    {
+        Map<Character, Node> child1 = new TreeMap<>();
+        Map<Character, Node> child2 = new TreeMap<>();
+        Map<Character, Node> child3 = new TreeMap<>();
+        Map<Character, Node> child4 = new TreeMap<>();
+
+        boolean aword = false;
+    }
+
+}
