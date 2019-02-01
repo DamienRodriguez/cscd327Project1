a2bc7acdb78909ff1ec57db4a1faa1e52862ce24
Damien Rodriguez <drodriguez17@eagles.ewu.edu>
2019-02-01 15:25:26 -0800

    dictionary tree created successfully

--- src/DictPrefixTree.java
+++ src/DictPrefixTree.java
@@ -18,18 +18,35 @@ public class DictPrefixTree
         insertString(root, s);
     }
 
-    private void insertString(Node root, String s) {
+    private void insertString(Node root, String s)
+    {
         Node cur = root;
-        for (char ch : s.toCharArray()) {
+        for (char ch : s.toCharArray())
+        {
+
             Node next = cur.children.get(ch);
+
             if (next == null)
                 cur.children.put(ch, next = new Node());
+
             cur = next;
         }
         cur.aword = true;
     }
 
-    
+    public void printSorted()
+    {
+        printSorted(this.root, "");
+    }
+
+    private void printSorted(Node n, String s)
+    {
+        if(n.aword)
+            System.out.println(s);
+
+        for(Character ch: n.children.keySet())
+            printSorted(n.children.get(ch), s + ch);
+    }
 
 
 
--- src/Tester.java
+++ src/Tester.java
@@ -1,7 +1,22 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
 public class Tester
 {
-    public static void main(String [] args)
+    public static void main(String [] args) throws Exception
     {
-        System.out.println("SNAFU");
+        File dictionary = new File("dictionary.txt");
+        Scanner fin = new Scanner(dictionary);
+
+        DictPrefixTree dictTree = new DictPrefixTree();
+
+        while(fin.hasNextLine())
+        {
+            String[] line = fin.nextLine().split(",");
+            dictTree.insertString(line[0]);
+        } //this should have the whole tree created
+
+       // dictTree.printSorted();       //uncomment this line to see that we successfully created our dictionary Tree
     }
 }
