7e599176b43e3c5959ea79b95fd6229eeb45e65d
RyanBucherl <rdbucherl17@gmail.com>
2019-02-01 15:31:55 -0800

    Update DictHashTable.java
    Wrote basic hashtable

--- src/DictHashTable.java
+++ src/DictHashTable.java
@@ -1,4 +1,34 @@
 public class DictHashTable
 {
-    //stuff goes here
-}
+    private class Record
+    {
+    	private Object key;
+    	private Object value;
+    	
+    	public Record(Object key, Object val)
+    	{
+    		this.key = key;
+    		this.value = val;
+    	}
+    	
+    	public boolean equals(Object obj)
+    	{
+    		if(obj instanceof(Record))
+    		{
+    			Record r = (Record)obj;
+    			return this.key.equals(r.key);
+    		}
+    		return false;
+    	}
+    }//END RECORD
+    
+    private int tableCapacity = 100000;
+    private int numElem;
+    private obj[] table;
+    
+    public DictHashTable(int capacity)
+    {
+    	this.table = new Object[capacity];
+    	this.numElem = 0;
+    }
+}//END DICTHASHTABLE
