ff5d13e89e8e27679b87d65cee089335f6c26dff
Damien Rodriguez <drodriguez17@eagles.ewu.edu>
2019-02-01 15:08:24 -0800

    can make dic

--- src/DictPrefixTree.java
+++ src/DictPrefixTree.java
@@ -1,4 +1,39 @@
+import java.util.*;
+
 public class DictPrefixTree
 {
+    private class Node
+    {
+        Map<Character, Node> children = new TreeMap<>();
+        boolean aword = false;
+    }
+
+    private Node root;
+    public DictPrefixTree()
+    {
+        this.root = new Node();
+    }
+
+    public void insertString(String s) {
+        insertString(root, s);
+    }
+
+    private void insertString(Node root, String s) {
+        Node cur = root;
+        for (char ch : s.toCharArray()) {
+            Node next = cur.children.get(ch);
+            if (next == null)
+                cur.children.put(ch, next = new Node());
+            cur = next;
+        }
+        cur.aword = true;
+    }
+
+    
+
+
+
+
+
     //stuff goes here
 }
