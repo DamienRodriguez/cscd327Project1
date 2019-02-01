b971e1eeb5dcbb48c07ae9db5ae51b2fd74f4c74
RyanBucherl <rdbucherl17@gmail.com>
2019-02-01 15:36:28 -0800

    Cleanup crew mk2

--- src/Tester.java
+++ src/Tester.java
@@ -3,7 +3,7 @@ import java.io.File;
 
 public class Tester
 {
-    public static void main(String[] args) throws Exception
+    public static void main(String args[]) throws Exception
     {
         File dictionary = new File("dictionary.txt");
         Scanner fin = new Scanner(dictionary);
