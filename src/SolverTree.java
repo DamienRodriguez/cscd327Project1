c1c81bb95f6159b5cff750a5a2f68a6f3e0709b9
RyanBucherl <rdbucherl17@gmail.com>
2019-02-03 15:35:40 -0800

    SolverTreeFiguredOut?Maybe?

--- src/SolverTree.java
+++ src/SolverTree.java
@@ -23,6 +23,15 @@ public class SolverTree
         	c4 = null;
         }
         
+        private Node(String s)
+        {
+        	letter = s;
+        	c1 = null;
+        	c2 = null;
+        	c3 = null;
+        	c4 = null;
+        }
+        
         private Node(String letter, Node n1, Node n2, Node n3 )
         {
         	this.letter = letter;
@@ -45,7 +54,7 @@ public class SolverTree
     private Node root;
     public SolverTree(String s) {
 
-        this.root = null;
+        this.root = new Node();
         // first root node
         // number 1
         // we would create 3 child nodes a, b, and c, then queue all of those into a queue
@@ -57,7 +66,9 @@ public class SolverTree
 
         // a, b, c
         // d e f, d e f, d e f
-
+        Queue temp = new Queue();
+        temp.enqueue(this.root);
+        constructorHelper(s, temp, 0);
     }
 
     private SolverTree.Node actualConstructor(final String s) {
@@ -75,8 +86,36 @@ public class SolverTree
 
     }
     
-    private Node constructorHelper(String s)
+    private void constructorHelper(String s, Queue wack, int level)
     {
-    	
-    }
+    	if(!(s.isEmpty()))
+    	{
+    		for(int x = 0; x < (int)(Math.pow(4,level)); x++)
+    		{
+    			Node temp = wack.dequeue();
+    			if(temp != null)
+    			{
+    				String set = keyPadLetters(Integer.parseInt(s.substring(0,1)) - 2);
+    				if(set.length == 3)
+    				{
+    					temp.c1 = new Node(set.substring(0,1));
+    					temp.c2 = new Node(set.substring(1,2));
+    					temp.c3 = new Node(set.substring(2));
+    				}
+    				else
+    				{
+    					temp.c1 = new Node(set.substring(0,1));
+    					temp.c2 = new Node(set.substring(1,2));
+    					temp.c3 = new Node(set.substring(2,3));
+    					temp.c4 = new Node(set.substring(3));
+    				}
+    				wack.enqueue(temp.c1);
+    				wack.enqueue(temp.c2);
+    				wack.enqueue(temp.c3);
+    				wack.enqueue(temp.c4);
+    			}//end Node creation/queueing
+    		}//end for loop
+    		constructorHelper(s.substring(1), wack, level + 1);
+    	}//end null string if statement
+    }//end constructorHelper
 }
